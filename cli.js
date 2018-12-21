e83673e20d12dee0a419973034320133f9521a59
kml1326 <kmlraj131996@gmail.com>
2018-12-21 16:03:08 +0530

    add comments

--- cli.js
+++ cli.js
@@ -1,13 +1,16 @@
+// imported require node module
 const http = require('http');
 const chalk = require('chalk');
 const logUpdate = require('log-update');
 const spinner = require('elegant-spinner')();
 
+// impor readline for reading data
 const readline = require('readline').createInterface({
   input : process.stdin,
   output : process.stdout
 })
 
+// set header
 const option = {
   header : {
     'User-Agent' : 'Weather-Info',
@@ -15,6 +18,8 @@ const option = {
   }
 }
 
+
+// this function for fetch weather api 
 function fetchWeather(place) {
   let apiKey = 'b20156877d92ec0e892a415edb752569';
 
@@ -36,16 +41,22 @@ function fetchWeather(place) {
       res.on('end', () => {
         resData = JSON.parse(resData);
         clearInterval(interval);
-        console.log(chalk.rgb(0, 0, 255).bold('Your Place name : ') + chalk.rgb(220, 23, 207).bold(resData.name) + '\n');
-        console.log(chalk.rgb(0, 0, 255).bold('Weather : ') + chalk.rgb(220, 23, 207).bold(resData.weather[0].description) +'\n');
-        console.log(chalk.rgb(0, 0, 255).bold('Temp: ') + chalk.rgb(220, 23, 207).bold(parseInt(resData.main.temp - 273.15)+ ' C') +'\n');
-        console.log(chalk.rgb(0, 0, 255).bold('Min Tamp : ') + chalk.rgb(220, 23, 207).bold(parseInt(resData.main.temp_min - 273.15) + ' C') + ' \n');
-        console.log(chalk.rgb(0, 0, 255).bold('Max Temp : ') + chalk.rgb(220, 23, 207).bold(parseInt(resData.main.temp_max - 273.15) + ' C') + '\n');
+        displayInfo(resData);
       })
     }
   })
 }
 
+// displayInfo function for only console the information
+function displayInfo(data) {
+  console.log(chalk.rgb(0, 0, 255).bold('Your Place name : ') + chalk.rgb(220, 23, 207).bold(data.name) + '\n');
+  console.log(chalk.rgb(0, 0, 255).bold('Weather : ') + chalk.rgb(220, 23, 207).bold(data.weather[0].description) +'\n');
+  console.log(chalk.rgb(0, 0, 255).bold('Temp: ') + chalk.rgb(220, 23, 207).bold(parseInt(data.main.temp - 273.15)+ ' C') +'\n');
+  console.log(chalk.rgb(0, 0, 255).bold('Min Tamp : ') + chalk.rgb(220, 23, 207).bold(parseInt(data.main.temp_min - 273.15) + ' C') + ' \n');
+  console.log(chalk.rgb(0, 0, 255).bold('Max Temp : ') + chalk.rgb(220, 23, 207).bold(parseInt(data.main.temp_max - 273.15) + ' C') + '\n');
+}
+
+// here take input from user using readline.question
 readline.question(chalk.rgb(66, 244, 116).bold('\n\n Enter your palce name : '), (placeName) => {
 
   fetchWeather(placeName);
